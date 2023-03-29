e936d1020da5ea276a34b95338cbaba64c6d8c8d
Tran Hoan <tranngocviethoan@gmail.com>
2023-03-29 16:59:15 +0700

    first commit

---
+++ Btaplon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Btaplon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
